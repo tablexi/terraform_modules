074aa95833f2b4c2b8409099f132a7b811eef881
Lloyd Philbrook <lloyd@tablexi.com>
2017-11-01 17:45:34 -0500

    Add variable descriptions

--- vpc/subnets/variables.tf
+++ vpc/subnets/variables.tf
@@ -1,18 +1,36 @@
-variable "name" { }
+variable "name" {
+  description = "The name of the subnet group. Also, used to tag the subnets."
+}
+
 variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
+  description = "The IPv4 network range for the VPC, in CIDR notation."
 }
-variable "vpc_id" { }
-variable "internet_gateway_id" { }
+
+variable "vpc_id" {
+  description = "The unique ID of the VPC."
+}
+
+variable "internet_gateway_id" {
+  description = "The unique ID of the Internet gateway."
+}
+
 variable "private_newbits" {
   default = 8
+  description = "The private subnets modifier of a routing mask or decrease the scope of the cidr_block by the given bits."
 }
+
 variable "private_netnum_offset" {
   default = 10
+  description = "Offset the private subnets netnum by a specific amount."
 }
+
 variable "public_newbits" {
   default = 8
+  description = "The public subnets modifier of a routing mask or decrease the scope of the cidr_block by the given bits."
 }
+
 variable "public_netnum_offset" {
   default = 0
+  description = "Offset the public subnets netnum by a specific amount."
 }
