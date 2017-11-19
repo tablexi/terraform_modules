2f37d8cacfface5d62f100ec37e79aae9bf647b4
Lloyd Philbrook <lloyd@tablexi.com>
2017-11-18 18:08:31 -0600

    ADD rds parameter group provided var

--- rds/example/parameter_group_provided.tf
+++ rds/example/parameter_group_provided.tf
@@ -4,6 +4,7 @@ module "rds-parameter-group-provided" {
   engine = "postgres"
   name = "rds-parameter-group-provided"
   parameter_group_name = "${aws_db_parameter_group.parameter-group-provided.id}"
+  parameter_group_provided = true
   subnets = ["rds-parameter-group-provided"]
   version = "9.6"
   vpc_id = "rds-parameter-group-provided"
