431d5911b4306a5eab303b5cf0115f063b0a40f5
stephen-puiszis <spuiszis@gmail.com>
2018-01-11 16:08:46 -0600

    Add an ses iam policy - WIP

---
+++ aws/iam/policies/ses_group/main.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_group" "mod" {
+  name = "app_servers"
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "mod" {
+    name = "AmazonSesSendingAccess"
+    group = "${aws_iam_group.mod.id}"
+    policy = <<JSON
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "ses:SendRawEmail",
+              "ses:SendEmail"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+JSON
+}
+
+resource "aws_iam_group_membership" "mod" {
+  name = "app-server-group-membership"
+  users = ["${var.users}"]
+  group = "${aws_iam_group.mod.name}"
+}
