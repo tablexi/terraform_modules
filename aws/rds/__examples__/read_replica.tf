05ce01ecac50afd32c39ae6aa3d850cf4e85edc0
Vladimir Andrijevikj <vladimir@andrijevik.net>
2018-02-23 11:36:54 -0600

    Don’t set db_subnet_group_name for RDS read replicas

---
+++ aws/rds/__examples__/read_replica.tf
@@ -0,0 +1,10 @@
+module "read-replica" {
+  source = "../"
+
+  engine = "postgres"
+  engine_version = "9.6"
+  name = "rds-postgres"
+  subnets = ["rds-postgres"]
+  vpc_id = "rds-postgres"
+  source_db = "primary-db-id"
+}
