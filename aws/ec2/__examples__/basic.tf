385e6d22dbad0f8ea565c4d548e9481cd59250ff
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-04-20 12:05:08 +0200

    Create security group for SSH ingress and all egress on EC2 instances

--- aws/ec2/__examples__/basic.tf
+++ aws/ec2/__examples__/basic.tf
@@ -4,6 +4,7 @@ module "without-ami" {
   key_name = "without-ami"
   name = "without-ami"
   subnets = ["without-ami"]
+  vpc_id = "vpc_abc123"
 }
 
 module "with-ami" {
@@ -13,4 +14,5 @@ module "with-ami" {
   key_name = "with-ami"
   name = "with-ami"
   subnets = ["with-ami"]
+  vpc_id = "vpc_abc123"
 }
