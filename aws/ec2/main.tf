33e1f8f53dfe99945c8dd908f9e0ea3f9fad4937
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-04-24 12:37:30 +0200

    Manage security_group_on_instances via aws_security_group_rule resources
    If we specify ingress and egress blocks in the definition of the aws_security_group inline, any rules attached via aws_security_group_rule resources elsewhere would be overwritten when this security group gets applied.
    
    By moving the predefined rules to their own aws_security_group_rule resources, we enable this security group to be used with the application_load_balancer module, which attaches rules to allow web ingress on the instances from the load balancer.
    
    For more details, see the note at the top of https://www.terraform.io/docs/providers/aws/r/security_group.html.

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -9,62 +9,69 @@ data "aws_ami" "mod" {
 }
 
 resource "aws_instance" "mod" {
-  count = "${var.count}"
-  ami = "${var.ami != "" ? var.ami : data.aws_ami.mod.id}"
-  instance_type = "${var.type}"
-  key_name = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.security_group_on_instances.id}", "${var.vpc_security_group_ids}"]
-  subnet_id = "${element(var.subnets, count.index)}"
+  count                       = "${var.count}"
+  ami                         = "${var.ami != "" ? var.ami : data.aws_ami.mod.id}"
+  instance_type               = "${var.type}"
+  key_name                    = "${var.key_name}"
+  vpc_security_group_ids      = ["${aws_security_group.security_group_on_instances.id}", "${var.vpc_security_group_ids}"]
+  subnet_id                   = "${element(var.subnets, count.index)}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
 
   tags {
-    Name = "${format("%s%02d", var.name, count.index + var.name_tag_starting_count)}"
+    Name        = "${format("%s%02d", var.name, count.index + var.name_tag_starting_count)}"
     Environment = "${var.env}"
   }
 
   ebs_optimized = "${var.ebs_optimized}"
+
   root_block_device {
     volume_type = "${var.root_block_type}"
     volume_size = "${var.root_block_size}"
   }
 
   lifecycle {
-    ignore_changes = [ "key_name" ]
+    ignore_changes = ["key_name"]
   }
 }
 
 resource "aws_eip" "mod" {
-  count = "${var.enable_eip ? var.count : 0}"
+  count    = "${var.enable_eip ? var.count : 0}"
   instance = "${element(aws_instance.mod.*.id, count.index)}"
-  vpc = true
+  vpc      = true
 }
 
 resource "aws_security_group" "security_group_on_instances" {
   name   = "${var.name}-ec2-instances"
   vpc_id = "${var.vpc_id}"
 
-  ingress {
-    from_port = 0
-    protocol  = "-1"
-    self      = true
-    to_port   = 0
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-  }
-
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-  }
-
   tags {
     "Name" = "${var.name}-ec2-instances"
   }
+}
+
+resource "aws_security_group_rule" "all_egress_on_instances_to_anywhere" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  protocol          = -1
+  security_group_id = "${aws_security_group.security_group_on_instances.id}"
+  to_port           = 0
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "all_ingress_on_instances_from_self" {
+  from_port         = 0
+  protocol          = -1
+  security_group_id = "${aws_security_group.security_group_on_instances.id}"
+  self              = true
+  to_port           = 0
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "ssh_ingress_on_instances_from_anywhere" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.security_group_on_instances.id}"
+  to_port           = 22
+  type              = "ingress"
 }
