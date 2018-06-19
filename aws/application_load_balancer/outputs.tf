47fad74fa24c4b93735004310e306932aef17c78
Lloyd Philbrook <lloyd@tablexi.com>
2018-06-19 07:47:57 +0000

    ADD http/s listener arn output

--- aws/application_load_balancer/outputs.tf
+++ aws/application_load_balancer/outputs.tf
@@ -2,6 +2,14 @@ output "dns_name" {
   value = "${module.load_balancer.dns_name}"
 }
 
+output "http_listener_arn" {
+  value = "${aws_alb_listener.http_listener.arn}"
+}
+
+output "https_listener_arn" {
+  value = "${aws_alb_listener.https_listener.arn}"
+}
+
 output "load_balancer_arn" {
   value = "${module.load_balancer.arn}"
 }
