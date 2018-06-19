4c699630e2cbd3d24d48d3d0edede2a5cebe1f5c
Lloyd Philbrook <lloyd@tablexi.com>
2018-06-19 07:27:35 +0000

    ADD load_balancer_arn output

--- aws/application_load_balancer/outputs.tf
+++ aws/application_load_balancer/outputs.tf
@@ -2,6 +2,10 @@ output "dns_name" {
   value = "${module.load_balancer.dns_name}"
 }
 
+output "load_balancer_arn" {
+  value = "${module.load_balancer.arn}"
+}
+
 output "zone_id" {
   value = "${module.load_balancer.zone_id}"
 }
