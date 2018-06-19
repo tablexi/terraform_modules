9ced4ed95f1139fa6b528bf7ec5f7dab4b06bc30
Lloyd Philbrook <lloyd@tablexi.com>
2018-06-19 07:27:51 +0000

    ADD load_balancer security group id

--- aws/application_load_balancer/outputs.tf
+++ aws/application_load_balancer/outputs.tf
@@ -6,6 +6,10 @@ output "load_balancer_arn" {
   value = "${module.load_balancer.arn}"
 }
 
+output "security_group_on_load_balancer_id" {
+  value = "${aws_security_group.security_group_on_load_balancer.id}"
+}
+
 output "zone_id" {
   value = "${module.load_balancer.zone_id}"
 }
