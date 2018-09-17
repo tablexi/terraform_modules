82f4a56caeea6f7f2383732b9f44e84e915fd025
Lloyd Philbrook <lloyd@tablexi.com>
2018-09-17 18:04:20 +0000

    ADD cloudtrail module

---
+++ aws/cloudtrail/main.tf
@@ -0,0 +1,42 @@
+resource "aws_cloudtrail" "mod" {
+  name                          = "${var.name}"
+  s3_bucket_name                = "${aws_s3_bucket.mod.id}"
+  include_global_service_events = true
+  enable_logging                = true
+}
+
+reource "aws_s3_bucket" "mod" {
+  bucket     = "${var.name}-cloudtrail"
+  acl_policy = "private"
+
+  policy = <<-JSON
+	{
+			"Version": "2012-10-17",
+			"Statement": [
+					{
+							"Sid": "AWSCloudTrailAclCheck",
+							"Effect": "Allow",
+							"Principal": {
+								"Service": "cloudtrail.amazonaws.com"
+							},
+							"Action": "s3:GetBucketAcl",
+							"Resource": "arn:aws:s3:::${var.name}-cloudtrail"
+					},
+					{
+							"Sid": "AWSCloudTrailWrite",
+							"Effect": "Allow",
+							"Principal": {
+								"Service": "cloudtrail.amazonaws.com"
+							},
+							"Action": "s3:PutObject",
+							"Resource": "arn:aws:s3:::${var.name}-cloudtrail/*",
+							"Condition": {
+									"StringEquals": {
+											"s3:x-amz-acl": "bucket-owner-full-control"
+									}
+							}
+					}
+			]
+	}
+	JSON
+}
