f037cf4b7a0a885d63b5075c55ad18eb3bb7b26d
Lloyd Philbrook <lloyd@tablexi.com>
2018-10-28 06:14:13 +0000

    ADD route table private/public id to outputs

--- aws/vpc/subnets/outputs.tf
+++ aws/vpc/subnets/outputs.tf
@@ -5,3 +5,11 @@ output "private_subnets" {
 output "public_subnets" {
   value = ["${aws_subnet.mod_public.*.id}"]
 }
+
+output "private_route_table_id" {
+  value = "${aws_route_table.mod_private.id}"
+}
+
+output "public_route_table_id" {
+  value = "${aws_route_table.mod_public.id}"
+}
