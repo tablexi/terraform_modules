ce6eee1467de73f6d17c2f1901842aa8ca59e4ac
Patrick Turley <patrick.turley@gmail.com>
2019-02-19 19:12:41 +0100

    Convert subnets module to only create either public or private as specified (#100)
    * Convert subnets module to only create either public or private as specified
    
    * Expect the word private in the name if users want it there
    
    * Fix outputs

--- aws/vpc/subnets/outputs.tf
+++ aws/vpc/subnets/outputs.tf
@@ -1,19 +1,11 @@
-output "private_subnets" {
-  value = ["${aws_subnet.mod_private.*.id}"]
+output "subnets" {
+  value = ["${aws_subnet.mod.*.id}"]
 }
 
-output "public_subnets" {
-  value = ["${aws_subnet.mod_public.*.id}"]
+output "route_table_id" {
+  value = "${aws_route_table.mod.*.id}"
 }
 
-output "private_route_table_id" {
-  value = "${aws_route_table.mod_private.*.id}"
-}
-
-output "public_route_table_id" {
-  value = "${aws_route_table.mod_public.*.id}"
-}
-
-output "public_subnets_by_az" {
-  value = "${zipmap(data.aws_availability_zones.available.names, aws_subnet.mod_public.*.id)}"
+output "subnets_by_az" {
+  value = "${zipmap(data.aws_availability_zones.available.names, aws_subnet.mod.*.id)}"
 }
