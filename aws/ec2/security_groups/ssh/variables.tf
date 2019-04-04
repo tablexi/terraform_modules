429c2388809c0d3d7bc4ce70bc43047472c90946
Lloyd Philbrook <lloyd@tablexi.com>
2019-04-04 16:48:00 -0500

    ADD the variable

--- aws/ec2/security_groups/ssh/variables.tf
+++ aws/ec2/security_groups/ssh/variables.tf
@@ -18,4 +18,9 @@ variable "ingress_cidr_blocks" {
   description = "List of cidr blocks to allow incoming access to on port 22."
 }
 
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the resources"
+  type        = "map"
+}
+
 variable "vpc_id" {}
