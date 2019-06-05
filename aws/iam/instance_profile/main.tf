3f163a247af8493ef82db9d3d8539ef78795e289
Patrick Turley <turley@tablexi.com>
2019-06-05 16:32:29 -0500

    Create instance profile module

---
+++ aws/iam/instance_profile/main.tf
@@ -0,0 +1,77 @@
+resource "aws_iam_instance_profile" "mod" {
+  name = "${var.name}"
+  role = "${aws_iam_role.mod.name}"
+}
+
+resource "aws_iam_role" "mod" {
+  assume_role_policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow"
+        }
+      ]
+    }
+    EOF
+
+  name = "${var.name}"
+  path = "/"
+}
+
+resource "aws_iam_role_policy" "mod-ses-role-policy" {
+  name  = "${var.name}-ses"
+  count = "${var.ses ? 1 : 0}"
+
+  policy = <<JSON
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                  "ses:SendRawEmail",
+                  "ses:SendEmail"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+  JSON
+
+  role = "${aws_iam_role.mod.id}"
+}
+
+resource "aws_iam_role_policy" "mod-s3-role-policy" {
+  name  = "${var.name}-s3"
+  count = "${var.s3_bucket != "" ? 1 : 0}"
+
+  policy = <<JSON
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": [
+            "s3:ListAllMyBuckets"
+          ],
+          "Effect": "Allow",
+          "Resource": "arn:aws:s3:::*"
+        },
+        {
+          "Action": "s3:*",
+          "Effect": "Allow",
+          "Resource": [
+            "arn:aws:s3:::${var.s3_bucket}",
+            "arn:aws:s3:::${var.s3_bucket}/*"
+          ]
+        }
+      ]
+    }
+  JSON
+
+  role = "${aws_iam_role.mod.id}"
+}
---
+++ aws/iam/instance_profile/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = "string"
+}
+
+variable "ses" {
+  type = "bool"
+  default = false
+  description = "Provide this instance with access to send mail via SES"
+}
+
+variable "s3_bucket" {
+  type = "string"
+  default = ""
+  description = "Provide this instance with access to the bucket"
+}
