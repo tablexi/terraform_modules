75a47d13d7ea3db9483f09e5dce0cb849d186e29
Patrick Turley <turley@tablexi.com>
2019-06-06 09:55:33 -0500

    Create instance profile module (#127)
    * Create instance profile module
    
    * Fix fmt
    
    * Remove type...cause we dont know how they work
    
    * Update heredoc
    
    * Update heredoc
    
    * Update heredoc
    
    * Update heredoc

---
+++ aws/iam/instance_profile/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type = "string"
+}
+
+variable "ses" {
+  default     = false
+  description = "Provide this instance with access to send mail via SES"
+}
+
+variable "s3_bucket" {
+  type        = "string"
+  default     = ""
+  description = "Provide this instance with access to the bucket"
+}
