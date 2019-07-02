11b74d40c13af91cf0ffff7e3d1d5b7193f2f357
Lloyd Philbrook <lloyd@tablexi.com>
2019-07-02 14:33:02 -0500

    UPDATE output based on merging https://github.com/hashicorp/terraform/issues/12453

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -1,14 +1,9 @@
 output "instance_ids" {
-  value = [aws_instance.mod[*].id]
+  value = aws_instance.mod[*].id
 }
 
 output "public_ips" {
-  # Cannot use lists in a conditional statement
-  # https://github.com/hashicorp/terraform/issues/12453
-  value = [split(
-    ",",
-    var.enable_eip ? join(",", aws_eip.mod[*].public_ip) : join(",", aws_instance.mod[*].public_ip),
-  )]
+  value = var.enable_eip ? aws_eip.mod[*].public_ip : aws_instance.mod[*].public_ip
 }
 
 output "security_group_on_instances" {
