efa096274e143133d19c5c3c8b597e93416daf0c
Patrick Turley <turley@tablexi.com>
2019-07-23 13:55:49 -0500

    Change caching strategy for static sites such that only the static directory is cached

--- aws/static_site/main.tf
+++ aws/static_site/main.tf
@@ -43,11 +43,29 @@ resource "aws_cloudfront_distribution" "mod" {
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
 
+  ordered_cache_behavior {
+    path_pattern     = "static/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${local.s3_origin_id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "${local.s3_origin_id}"
     viewer_protocol_policy = "redirect-to-https"
+    default_ttl            = 0
+    max_ttl                = 0
 
     forwarded_values {
       query_string = true
@@ -63,15 +81,12 @@ resource "aws_cloudfront_distribution" "mod" {
       restriction_type = "none"
     }
   }
-
   tags = "${var.tags}"
-
   viewer_certificate {
     acm_certificate_arn      = "${var.acm_certificate_arn}"
     minimum_protocol_version = "TLSv1.2_2018"
     ssl_support_method       = "sni-only"
   }
-
   // Allow for React Router to work using the vanity domains
   custom_error_response {
     error_caching_min_ttl = "300"
@@ -79,7 +94,6 @@ resource "aws_cloudfront_distribution" "mod" {
     response_code         = "200"
     response_page_path    = "/index.html"
   }
-
   custom_error_response {
     error_caching_min_ttl = "300"
     error_code            = "404"
