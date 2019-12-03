dd17050c08fa8cc5897ccb05fd0c29474425f375
Vladimir Andrijevikj <vlad@tablexi.com>
2019-12-03 13:25:32 +0100

    Eliminate unnecessary default_az_to_netnum in aws/vpc/subnets

--- aws/vpc/subnets/main.tf
+++ aws/vpc/subnets/main.tf
@@ -7,26 +7,6 @@ data "aws_vpc" "current" {
   id = "${var.vpc_id}"
 }
 
-locals {
-  default_az_to_netnum = {
-    a = 1
-    b = 2
-    c = 3
-    d = 4
-    e = 5
-    f = 6
-  }
-}
-
-data "template_file" "az_to_netnum" {
-  count    = "${length(data.aws_availability_zones.available.names)}"
-  template = "$${netnum}"
-
-  vars {
-    netnum = "${lookup(local.default_az_to_netnum, substr(element(data.aws_availability_zones.available.names, count.index), -1, 1))}"
-  }
-}
-
 resource "aws_route_table" "mod" {
   tags   = "${var.tags}"
   vpc_id = "${var.vpc_id}"
@@ -42,7 +22,7 @@ resource "aws_route" "mod" {
 resource "aws_subnet" "mod" {
   count                   = "${length(data.aws_availability_zones.available.names)}"
   availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
-  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block, var.newbits, var.netnum_offset + element(data.template_file.az_to_netnum.*.rendered, count.index))}"
+  cidr_block              = "${cidrsubnet(data.aws_vpc.current.cidr_block, var.newbits, var.netnum_offset + count.index + 1)}"
   map_public_ip_on_launch = "${var.public}"
   tags                    = "${var.tags}"
   vpc_id                  = "${var.vpc_id}"
