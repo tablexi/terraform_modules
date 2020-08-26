b679c6d1eb4944e5969a4b2b5b237768c45351b1
Jason Hanggi <jason@tablexi.com>
2020-08-26 11:37:46 -0500

    Add ability to attach ses sending to roles in addition to users

--- aws/iam/ses_send/main.tf
+++ aws/iam/ses_send/main.tf
@@ -2,30 +2,36 @@ resource "aws_iam_group" "mod" {
   name = "ses_senders"
 }
 
+data "aws_iam_policy_document" "mod" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:SendRawEmail",
+      "ses:SendEmail",
+    ]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_group_policy" "mod" {
   name  = "AmazonSesSendingAccess"
   group = aws_iam_group.mod.id
 
-  policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = [
-            "ses:SendRawEmail",
-            "ses:SendEmail",
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-      Version = "2012-10-17"
-    }
-  )
+  policy = data.aws_iam_policy_document.mod.json
 }
 
 resource "aws_iam_group_membership" "mod" {
+  count = length(var.users)  > 0 ? 1 : 0
   name  = "app-server-group-membership"
   users = var.users
   group = aws_iam_group.mod.name
+}
+
+resource "aws_iam_role_policy" "mod" {
+  count      = length(var.roles)
+  name       = "AmazonSesSendingAccess"
+  policy     = data.aws_iam_policy_document.mod.json
+  role       = var.roles[count.index]
 }
 
