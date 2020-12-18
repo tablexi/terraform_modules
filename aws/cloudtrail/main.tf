17443971de3f11ce87e149146ff348d95d787ec0
Andrew Horner <andrew@tablexi.com>
2020-12-18 11:06:07 -0700

    Spin up CloudWatch for CloudTrail logs
    As a first step toward improving our AWS security defaults, we'd like
    to enable CloudWatch any time we're spinning up a CloudTrail trail.
    This will give us a lot of freedom moving forward to set up alerts,
    monitoring, et cetera.
    
    As part of this change, we're also making a few tweaks to our defaults
    for the `cloudtrail` module:
    
    - Enable logging for the CloudTrail log bucket
    - Enable log file validation for CloudTrail logs
    - Use an `aws_iam_policy_document` data source for our bucket policy
    
    There are a few best practices we're _not_ going to try to implement
    right now -- encrypting our log bucket with a KMS key, for instance.
    But this should be a good step in the right direction to make security
    a first-class citizen of our infrastructure management.

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -3,44 +3,102 @@ resource "aws_cloudtrail" "mod" {
   s3_bucket_name                = aws_s3_bucket.mod.id
   include_global_service_events = true
   enable_logging                = true
+  enable_log_file_validation    = true
+
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.mod.arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudwatch.arn
+
   tags                          = var.tags
 }
 
 resource "aws_s3_bucket" "mod" {
   bucket = "${var.name}-cloudtrail"
   acl    = "private"
+  policy = data.aws_iam_policy_document.s3.json
 
-  policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = "s3:GetBucketAcl"
-          Effect = "Allow"
-          Principal = {
-            Service = "cloudtrail.amazonaws.com"
-          }
-          Resource = "arn:aws:s3:::${var.name}-cloudtrail"
-          Sid = "AWSCloudTrailAclCheck"
-        },
-        {
-          Action = "s3:PutObject"
-          Condition = {
-            StringEquals = {
-              "s3:x-amz-acl" = "bucket-owner-full-control"
-            }
-          }
-          Effect = "Allow"
-          Principal = {
-            Service = "cloudtrail.amazonaws.com"
-          }
-          Resource = "arn:aws:s3:::${var.name}-cloudtrail/*"
-          Sid = "AWSCloudTrailWrite"
-        },
-      ]
-      Version = "2012-10-17"
-    }
-  )
+  tags   = var.tags
 
-  tags = var.tags
+  logging {
+    target_bucket = "cloudtrail-logs"
+    target_prefix = var.name
+  }
 }
 
+resource "aws_cloudwatch_log_group" "mod" {
+  name = "${var.name}-cloudtrail-logs"
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  name               = "${var.name}-cloudwatch"
+  path               = "/service-role/"
+  description        = "${var.name} cloudwatch logs role"
+  assume_role_policy = data.aws_iam_policy_document.assume-role.json
+
+  tags               = var.tags
+}
+
+resource "aws_iam_policy" "cloudwatch" {
+  name        = "${var.name}-cloudwatch-policy"
+  description = "${var.name} cloudwatch logs policy"
+  policy      = data.aws_iam_policy_document.cloudwatch.json
+}
+
+resource "aws_iam_policy_attachment" "mod" {
+  name       = "${var.name}-cloudwatch-attachment"
+  roles      = [aws_iam_role.cloudwatch.name]
+  policy_arn = aws_iam_policy.cloudwatch.arn
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid       = "AWSCloudTrailAclCheck"
+    actions   = ["s3:GetBucketAcl"]
+    resources = ["arn:aws:s3:::${var.name}-cloudtrail"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+  statement {
+    sid       = "AWSCloudTrailWrite"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${var.name}-cloudtrail/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "assume-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch" {
+  statement {
+    sid       = "AWSCloudTrailCreateLogStream"
+    actions   = ["logs:CreateLogStream"]
+    resources = ["${aws_cloudwatch_log_group.mod.arn}:*"]
+  }
+
+  statement {
+    sid       = "AWSCloudTrailPutLogEvents"
+    actions   = ["logs:PutLogEvents"]
+    resources = ["${aws_cloudwatch_log_group.mod.arn}:*"]
+  }
+}
