845e264258d6d2a0dec2a09d9c3bdba4e07306b8
Derek Hammer <hammer@tablexi.com>
2021-02-05 15:06:11 -0700

    Output worker sg

--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -18,6 +18,10 @@ output "node_instance_role" {
   value = aws_iam_role.nodes.arn
 }
 
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.master.vpc_config[0].cluster_security_group_id
+}
+
 output "oidc_provider" {
   value = aws_iam_openid_connect_provider.default.arn
 }
