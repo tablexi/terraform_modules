3bd6d668fc9daaca55e4cd87331d788a82f3327b
L Philbrook <lloyd@tablexi.com>
2022-10-04 10:02:03 -0500

    ADD parameter group id output to elasticache module

--- aws/elasticache/outputs.tf
+++ aws/elasticache/outputs.tf
@@ -2,3 +2,6 @@ output "sg_on_elasticache_instance_id" {
   value = aws_security_group.sg_on_elasticache_instance.id
 }
 
+output "elasticache_parameter_group_id" {
+  value = var.create_parameter_group ? aws_elasticache_parameter_group.mod[0].id : null
+}
