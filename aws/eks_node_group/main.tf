01d1273e0488ff14e1584f6b251775d9cbf0a6db
Amanda Snyder <asnyder@tablexi.com>
2022-10-14 11:03:50 -0600

    using dynamic block so remote access isn't always included

--- aws/eks_node_group/main.tf
+++ aws/eks_node_group/main.tf
@@ -19,8 +19,13 @@ resource "aws_eks_node_group" "nodes" {
   node_role_arn          = var.node_iam_role.arn
   tags                   = local.node_group_tags
 
-  remote_access {
-    ec2_ssh_key = var.ec2_ssh_key
+  dynamic "ec2_ssh_key" {
+    for_each = var.ec2_ssh_key
+    content {
+      remote_access = {
+        ec2_ssh_key = var.ec2_ssh_key
+      }
+    }
   }
 
   scaling_config {
