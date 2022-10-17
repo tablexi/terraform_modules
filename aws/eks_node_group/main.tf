6c207b323e38ef6f9629b7daa1f900a6833e7da1
Amanda Snyder <asnyder@tablexi.com>
2022-10-17 11:16:51 -0600

    using ternary to get remote access

--- aws/eks_node_group/main.tf
+++ aws/eks_node_group/main.tf
@@ -19,12 +19,9 @@ resource "aws_eks_node_group" "nodes" {
   node_role_arn          = var.node_iam_role.arn
   tags                   = local.node_group_tags
 
-  dynamic "remote_access" {
-    for_each = var.ec2_ssh_key
-    content {
-      ec2_ssh_key = var.ec2_ssh_key
-    }
-  }
+  remote_access = var.ec2_ssh_key ? {
+    ec2_ssh_key = var.ec2_ssh_key
+  } : {}
 
   scaling_config {
     desired_size = var.desired_size
