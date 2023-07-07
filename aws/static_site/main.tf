ef02f14701260d05ad8c2fd95603d99518e8efb5
L Philbrook <lloyd@tablexi.com>
2023-07-07 11:54:23 -0500

    ADD server_side_encryption_configuration rule to static site module

--- aws/static_site/main.tf
+++ aws/static_site/main.tf
@@ -40,6 +40,15 @@ resource "aws_s3_bucket" "mod" {
     index_document = var.index_document
     error_document = var.index_document
   }
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = false
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 locals {
